7d7fd2daac2acc3259f9b5393f1f89d58b88c740
Shino <dg047359@gmail.com>
2023-10-10 21:14:26 -0700

    Co-authored-by: KikeDeAlba <KikeDeAlba@users.noreply.github.com> Co-authored-by: JesusOlivas12 <JesusOlivas12@users.noreply.github.com>

--- schemas/db.sql
+++ schemas/db.sql
@@ -1,1 +1,55 @@
-CREATE IF NOT EXISTS DATABASE taqueria_db;
+-- Active: 1696921476499@@127.0.0.1@3306
+-- SQLBook: Code
+CREATE DATABASE IF NOT EXISTS taqueria_db;
+USE taqueria_db;
+
+CREATE TABLE IF NOT EXISTS products(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS type_order (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO type_order (name) VALUES
+    ('domicilio'),
+    ('comenzal');
+
+CREATE TABLE IF NOT EXISTS status_order (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO status_order (name) VALUES
+    ('pendiente'),
+    ('cancelada'),
+    ('pagada');
+
+CREATE TABLE IF NOT EXISTS orders (
+    id INT NOT NULL AUTO_INCREMENT,
+    created_date DATETIME NOT NULL DEFAULT (NOW()),
+    client_name VARCHAR(50) NOT NULL,
+    adress VARCHAR(50),
+    status_id INT NOT NULL DEFAULT (1),
+    type_id INT NOT NULL,
+    table_number INT NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY (status_id) REFERENCES status_order(id),
+    FOREIGN KEY (type_id) REFERENCES type_order(id)
+);
+
+CREATE TABLE IF NOT EXISTS order_products (
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY(order_id, product_id),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id),
+    CHECK (quantity > 0)
+);
