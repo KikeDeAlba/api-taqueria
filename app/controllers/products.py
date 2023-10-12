ade013ee1f12937d924d508da98a30fc3234aab0
Shino <shinogasia@gmail.com>
2023-10-11 20:30:32 -0700

    Co-authored-by: KikeDeAlba <KikeDeAlba@users.noreply.github.com>

--- app/controllers/products.py
+++ app/controllers/products.py
@@ -1,4 +1,6 @@
 from services.db import taqueria_db
+from models.product import Product
+
 
 # Esta funcion debe traer todos los productos de la base de datos y devolverlos
 def get_all_products():
@@ -17,5 +19,34 @@ def get_one_product(id: int):
 
     if not product:
         raise Exception(f"Product with id {id} not found")
-    
+
     return product
+
+# El nombre de la funcion que lo que hace el contrato
+# Lo que hay entre parentesis es lo que se necesita para llevar acabo el contrato
+def insert_product(product: Product):
+    taqueria_db.insert(
+        table="products",
+        data={
+            "name": product.name,
+            "price": product.price,
+        }
+    )
+    
+    return {
+        "message": "Product created"
+    }
+
+def update_product(id: int, product: Product):
+    taqueria_db.update(
+        table="products",
+        where=f"id = {id}",
+        data={
+            "name": product.name,
+            "price": product.price,
+        }
+    )
+
+    return {
+        "message": "Product updated"
+    }
